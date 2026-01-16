3151ffa9fee902d376f310b10253644f6f8c5d77
Karin <karinversan@gmail.com>
2026-01-17 01:26:57 +0300

    update

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5e6806618775a08a"; }
+/******/ 		__webpack_require__.h = function() { return "0b73386c7dd53a85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
